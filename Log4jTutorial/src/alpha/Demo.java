b3a4af977b1a26eedaaa70a0026cc84aa5883c02
AnilTaman <tamang.anil3@gmail.com>
2020-04-18 16:19:22 +0530

    in branch

--- Log4jTutorial/src/alpha/Demo.java
+++ Log4jTutorial/src/alpha/Demo.java
@@ -9,10 +9,10 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Log.debug("I have click on button");		//Anil Tamang code	
-		Log.info("Button is displayed");
+		Log.debug("I have click on button");		
+		Log.info("Button is displayed");	
 		Log.error("Button is not displayed");			
-		Log.fatal("Button is missing");		
+			
 	}
 
 }
